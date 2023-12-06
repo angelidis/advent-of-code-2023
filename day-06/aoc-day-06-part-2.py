137b70de00d4d5922c0e10a0edf93ea7e10b8a7b
angelidis <nicholas.angelidis@gmail.com>
2023-12-06 12:23:22 +0200

    day 06 part 2

---
+++ day-06/aoc-day-06-part-2.py
@@ -0,0 +1,31 @@
+# %% functions
+
+
+# %% main
+
+file_path = "input.txt"
+with open(file_path) as file:
+    lines = [line.rstrip() for line in file]
+
+
+times, distances = [list(map(int, line.replace(" ", "").split(":")[1].strip().split())) for line in lines]
+
+
+
+ans = 1 # product of how many we can win the race
+
+# hold_speed = hold x ms --> x mm/ms
+# distance traveled = rem_time * hold_speed
+
+for time, distance in zip(times, distances):
+    
+    margin = 0
+    for hold in range(time):
+        if hold * (time-hold) > distance:
+            margin +=1
+              
+    ans *= margin
+    print(margin)
+    
+
+print(f"ans -> {ans}")
